3225a973b8f4d3ac1198c968f37842662b23616a
Eric Groß <ericgross.eg@gmail.com>
2022-01-26 23:13:20 +0100

    Add reactive value

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,44 @@
-<script>
-	let firstName = 'Jimi';
-	let lastName = 'Hendrix';
-	let beltColor = 'black'
-	const handleClick = () => {
-	    beltColor = 'orange'
-	}
+<script >
+    let firstName = 'Jimi';
+    let lastName = 'Hendrix';
+    let beltColor: String = 'black';
 
-	const handeChangeEvent = (e) => {
-	    beltColor = e.target.value
-	}
+    $: fullName = `${firstName} ${lastName}`;
+
+    const handleClick = () => {
+        beltColor = 'orange';
+    };
+
+    const handeChangeEvent = (e) => {
+        beltColor = e.target.value;
+    };
 </script>
 
 <main>
-	<p>{beltColor} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={beltColor}>
+    <p>{fullName} {beltColor} belt</p>
+    <input type="text" bind:value={firstName}>
+    <input type="text" bind:value={lastName}>
+    <input type="text" bind:value={beltColor}>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
