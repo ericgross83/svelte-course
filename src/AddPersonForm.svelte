2f835fd0d861b5e78a2029761408db1817d64c94
Eric Groß <ericgross.eg@gmail.com>
2022-01-31 23:13:40 +0100

    Binding checkboxes with bind:group

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,11 +2,12 @@
     let name;
     let beltColour;
     let age;
-    let fighting = false;
-    let sneaking = false;
-    let running = false;
+    // let fighting = false;
+    // let sneaking = false;
+    // let running = false;
+    let skills = [];
     let handleSubmit = () => {
-        console.log(name, beltColour, age, fighting, sneaking, running);
+        console.log(name, beltColour, age, skills);
     };
 </script>
 
@@ -15,8 +16,8 @@
     <input type="text" placeholder="belt colour" bind:value={beltColour}>
     <input type="number" placeholder="age" bind:value={age}>
     <label>Skills:</label>
-    <input type="checkbox" bind:checked={fighting}> fighting <br>
-    <input type="checkbox" bind:checked={sneaking}> sneaking <br>
-    <input type="checkbox" bind:checked={running}> running <br>
+    <input type="checkbox" bind:group={skills} value="fighting"> fighting <br>
+    <input type="checkbox" bind:group={skills} value="sneaking"> sneaking <br>
+    <input type="checkbox" bind:group={skills} value="running"> running <br>
     <button>Add Person</button>
 </form>
