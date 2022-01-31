1f4b244e451d01f8c68067cf3fc8b63ffeb6ee8a
Eric Groß <ericgross.eg@gmail.com>
2022-01-31 23:44:07 +0100

    Add select and radio buttons and bind them with bind:group

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -5,7 +5,7 @@
     // let fighting = false;
     // let sneaking = false;
     // let running = false;
-    let skills = [];
+    let skills;
     let handleSubmit = () => {
         console.log(name, beltColour, age, skills);
     };
@@ -13,11 +13,20 @@
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
-    <input type="text" placeholder="belt colour" bind:value={beltColour}>
     <input type="number" placeholder="age" bind:value={age}>
     <label>Skills:</label>
-    <input type="checkbox" bind:group={skills} value="fighting"> fighting <br>
-    <input type="checkbox" bind:group={skills} value="sneaking"> sneaking <br>
-    <input type="checkbox" bind:group={skills} value="running"> running <br>
+    <label>
+        <input type="radio" bind:group={skills} value="fighting">
+        <span>fighting</span>
+    </label>
+    <input type="radio" bind:group={skills} value="sneaking"> sneaking <br>
+    <input type="radio" bind:group={skills} value="running"> running <br>
+    <label>Belt colour:</label>
+    <select bind:value={beltColour}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="brown">brown</option>
+        <option value="white">white</option>
+    </select>
     <button>Add Person</button>
 </form>
