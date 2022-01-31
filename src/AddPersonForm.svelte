9beed43812a02fdb12d5e1e6d2670e255c8c12c0
Eric Groß <ericgross.eg@gmail.com>
2022-01-31 23:10:38 +0100

    Binding checkboxes with bind:checked

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,8 +2,11 @@
     let name;
     let beltColour;
     let age;
+    let fighting = false;
+    let sneaking = false;
+    let running = false;
     let handleSubmit = () => {
-        console.log(name, beltColour, age);
+        console.log(name, beltColour, age, fighting, sneaking, running);
     };
 </script>
 
@@ -11,5 +14,9 @@
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="belt colour" bind:value={beltColour}>
     <input type="number" placeholder="age" bind:value={age}>
+    <label>Skills:</label>
+    <input type="checkbox" bind:checked={fighting}> fighting <br>
+    <input type="checkbox" bind:checked={sneaking}> sneaking <br>
+    <input type="checkbox" bind:checked={running}> running <br>
     <button>Add Person</button>
 </form>
