0289a453dc02f37ed62f19986ede32521984d5bb
Eric Groß <ericgross.eg@gmail.com>
2022-02-08 23:32:38 +0100

    Add lesson code

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    <div class="copyright">Copyright 2020 Poll Ninja</div>
+</footer>
+<style>
+    footer{
+        padding: 40px;
+        text-align: center;
+    }
+    .copyright {
+        color: #aaa;
+        font-size: 14px;
+        padding: 20px;
+        display: inline-block;
+        border-top: 1px solid #ddd;
+    }
+</style>
