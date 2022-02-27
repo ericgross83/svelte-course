66b5c79d258562990d745a2cb942e9d674d9103b
Eric Groß <ericgross.eg@gmail.com>
2022-02-27 17:56:34 +0100

    20-tabs-component

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,28 @@
 <script>
-	import Header from "./components/Header.svelte";
-	import Footer from "./components/Footer.svelte";
+    import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+    import Tabs from "./shared/Tabs.svelte";
+
+    //    tabs
+    let items = ['Current Polls', 'Add New Poll'];
+    let activeItem = 'Current Polls';
 </script>
 
 <Header />
 <main>
+    <Tabs {items} {activeItem} on:tabSelected={(e) => activeItem = e.detail} />
+    {#if activeItem === 'Current Polls'}
+        <p>Poll list component goes here</p>
+    {:else if activeItem === 'Add New Poll'}
+        <p>New Poll form Component goes here</p>
+    {/if}
 
 </main>
 
 <Footer />
 <style>
-	main {
-		max-width: 960px;
-		margin: 40px auto;
-	}
+    main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
 </style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import {createEventDispatcher} from 'svelte'
+
+    const dispatch = createEventDispatcher()
+
+    export let items;
+    export let activeItem;
+    const handleClick = (item) => {
+        dispatch('tabSelected', item)
+        console.log(item)
+    };
+
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item}
+            <li>
+                <div class:active={item === activeItem} on:click={() =>  handleClick(item)}>{item}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+
+    ul {
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    li {
+        margin: 0 16px;
+        font-size: 18px;
+        color: #555;
+        cursor: pointer;
+    }
+
+    .active {
+        color: #d91b42;
+        border-bottom: 2px solid #d91b42;
+        padding-bottom: 8px;
+    }
+
+</style>
